f9734e447f4a50ba4c4cdd3e368ab3b12aca82b9
Guillermo Andrés <22777232+gllrm-andres@users.noreply.github.com>
2021-10-06 23:32:07 -0500

    init app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import HomePage from "./homepage.component";
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/homepage.component.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const HomePage = () => (
+  <div className="max-w-4xl mx-auto mt-12">
+    <div className="grid grid-cols-3 gap-4">
+      <div className="w-full h-56 grid place-items-center border border-black">
+        <div className="p-4 border border-black text-center">
+          <div className="uppercase tracking-wide font-bold">Hats</div>
+          <div className="uppercase font-normal tracking-wide"> Shop now</div>
+        </div>
+      </div>
+      <div className="w-full h-56 grid place-items-center border border-black">
+        <div className="p-4 border border-black text-center">
+          <div className="uppercase tracking-wide font-bold">Jackets</div>
+          <div className="uppercase font-normal tracking-wide"> Shop now</div>
+        </div>
+      </div>
+      <div className="w-full h-56 grid place-items-center border border-black">
+        <div className="p-4 border border-black text-center">
+          <div className="uppercase tracking-wide font-bold">Sneackers</div>
+          <div className="uppercase font-normal tracking-wide"> Shop now</div>
+        </div>
+      </div>
+      <div className="w-full h-56 grid place-items-center border border-black">
+        <div className="p-4 border border-black text-center">
+          <div className="uppercase tracking-wide font-bold">Womens</div>
+          <div className="uppercase font-normal tracking-wide"> Shop now</div>
+        </div>
+      </div>
+      <div className="w-full h-56 grid place-items-center border border-black">
+        <div className="p-4 border border-black text-center">
+          <div className="uppercase tracking-wide font-bold">Mens</div>
+          <div className="uppercase font-normal tracking-wide"> Shop now</div>
+        </div>
+      </div>
+      <div className="w-full h-56 grid place-items-center border border-black">
+        <div className="p-4 border border-black text-center">
+          <div className="uppercase tracking-wide font-bold">Hats</div>
+          <div className="uppercase font-normal tracking-wide"> Shop now</div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
